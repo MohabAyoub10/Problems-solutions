4e22f5648906c506088e33042dbea0c0229d027f
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2020-12-01 19:22:59 +0200

    Create C - Ping-pong.cpp
    https://codeforces.com/contest/1455/problem/C
    My Submissions : https://codeforces.com/contest/1455/submission/100071786

---
+++ Codeforces/C - Ping-pong.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Fast_IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+int main()
+{
+    Fast_IO;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        cout<< --n <<" " << x <<"\n";
+    }
+}
