963da5842144e440296ce8a7d1978f3c1dbf6e9e
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2020-12-04 00:52:36 +0200

    Create 10611 - The Playboy Chimp.cpp
    https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1552

---
+++ UVA/10611 - The Playboy Chimp.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Fast_IO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+int n, q;
+int up(int arr[], int x) {
+  int f = 0, l = n - 1;
+  while (l >= f) {
+    int mid = (l + f) / 2;
+    if (arr[mid] <= x)
+      f = mid + 1;
+    else
+      l = mid - 1;
+  }
+  if (f >= n) return 0;
+  else return arr[f];
+}
+
+int down(int arr[], int x) {
+  int f = 0, l = n - 1;
+  while (l >= f) {
+    int mid = (l + f) / 2;
+    if (arr[mid] >= x)
+      l = mid - 1;
+    else
+      f = mid + 1;
+  }
+  if (l < 0) return 0;
+  else return arr[l];
+}
+
+int main() {
+  Fast_IO;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  cin >> q;
+
+  for (int i = 0; i < q; i++) {
+    int x;
+    cin >> x;
+    int UP = up(a, x), DOWN = down(a, x);
+    if (DOWN != 0 && UP != 0)
+      cout << DOWN << " " << UP << "\n";
+    else if (DOWN == 0)
+      cout << "X" << " " << UP << "\n";
+    else
+      cout << DOWN << " " << "X" << "\n";
+
+  }
+}
